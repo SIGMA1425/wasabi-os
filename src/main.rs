de73457ad1dbedce91b35ea9d41e5d7434f86571
sigma.vm <sigma@example.com>
2025-07-05 15:35:21 +0900

    ページング関連のデモ用コードの削除

--- src/main.rs
+++ src/main.rs
@@ -34,11 +34,7 @@ use wasabi::init::init_basic_runtime;
 use wasabi::print::hexdump;
 use wasabi::println;
 
-use wasabi::x86::flush_tlb;
 use wasabi::x86::init_exceptions;
-use wasabi::x86::read_cr3;
-use wasabi::x86::trigger_debug_interrupt;
-use wasabi::x86::PageAttr;
 
 use wasabi::hpet::global_timestamp;
 
@@ -71,36 +67,10 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     init_allocator(&memory_map);
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
 
-    let cr3 = wasabi::x86::read_cr3();
-    println!("cr3 = {cr3:#p}");
-    // hexdump(unsafe{ &*cr3});
-    let t = Some(unsafe { &*cr3 });
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-
     // 例外の初期化
     let (_gdt, _idt) = init_exceptions();
-    info!("Exception initialized!");
-    // INT3命令を実行する
-    // INT3命令実行時にsrc/x86.rs内で定義した例外ハンドラに処理を移す
-    // 例外ハンドラ実行後にIRET命令を実行して例外ハンドラの処理を終了
-    trigger_debug_interrupt();
-    info!("Exception continued");
-    init_paging(&memory_map);
-    info!("Now we are using our own page tables!");
 
-    let page_table = read_cr3();
-    unsafe {
-        (*page_table)
-            .create_mapping(0, 4096, 0, PageAttr::NotPresent)
-            .expect("Failed to unmap page 0");
-    }
-    flush_tlb();
+    init_paging(&memory_map);
 
     init_hpet(acpi);
     let t0 = global_timestamp();
